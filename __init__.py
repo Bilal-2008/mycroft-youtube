a8abb5ab489d2717cc5c7fabaa05136f887239b6
Bilal-2008 <65559552+Bilal-2008@users.noreply.github.com>
2021-12-28 18:55:28 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,34 +9,35 @@ class YoutubeSkill(MycroftSkill):
     def __init__(self):
         super().__init__()
     def initialize(self):
-        my_setting = self.settings.get('my_setting')
+        self.my_setting = self.settings.get('my_setting')
     @intent_handler('youtube.intent')
-    def youtube(self, message):
-        yt=message.data.get("name")
-        if yt==None:
+    def handle_youtube(self, message):
+        self.yt=message.data.get("name")
+        if self.yt==None:
             self.speak_dialog("Specify", wait=True)
             return False
-        self.speak_dialog("Search", data={"title":yt})
+        self.speak_dialog("Search", data={"title":self.yt})
         try:
-            vid=yt.replace(" ", "+")
-            url=f"https://www.youtube.com/results?search_query={vid}"
-            html=urlopen(url)
-            ids=re.findeall(r"watch\?v=(\S{11})", html.read().decode())
-            id=ids[0]
-            url=f"https://www.youtube.com/watch?v={id}"
+            self.vid=self.yt.replace(" ", "+")
+            self.url=f"https://www.youtube.com/results?search_query={self.vid}"
+            self.html=urlopen(self.url)
+            self.ids=re.findeall(r"watch\?v=(\S{11})", self.html.read().decode())
+            self.id=self.ids[0]
+            self.url=f"https://www.youtube.com/watch?v={self.id}"
             os.system("rm /home/pi/youtube.mp3")
-            ydl_opts={"format":"bestaudio/best","outtmpl":"/home/pi/youtube.wav","postprocessors":[{"key":"FFmpegExtractAudio","preferredcodec":"mp3","preferredquality":"192"}]}
+            self.ydl_opts={"format":"bestaudio/best","outtmpl":"/home/pi/youtube.wav","postprocessors":[{"key":"FFmpegExtractAudio","preferredcodec":"mp3","preferredquality":"192"}]}
             with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                ydl.download([url])
+                ydl.download([self.url])
             try:
                 self.media.stop()
             except:
                 pass
-            self.speak_dialog("Play", data={"title": yt}, wait=True)
+            self.speak_dialog("Play", data={"title": self.yt}, wait=True)
             self.media=vlc.MediaPlayer("/home/pi/youtube.mp3")
             self.media.play()
         except:
             self.speak_dialog("Error", wait=True)
+            
     def stop(self):
         self.media.stop()
 
